7f2fea35b076148287063432ee624c69f1c72b97
mrtiny <anthonydesponds@gmail.com>
2017-11-10 15:28:21 +0100

    Add files via upload

--- guiapp no2/src/guiapp/no2/fenetre.java
+++ guiapp no2/src/guiapp/no2/fenetre.java
@@ -2,6 +2,8 @@
 package guiapp.no2;
 
 import java.awt.*;
+import java.awt.event.*;
+import java.util.regex.*;
 import javax.swing.*;
 
  class fenetre extends JFrame {
@@ -27,13 +29,17 @@ import javax.swing.*;
      private JLabel kmh = new JLabel("kilometre/heure");
      private JLabel kn = new JLabel("knot");
      private JLabel test = new JLabel("test");
+     private JLabel top = new JLabel("aaaaa");
+     private JLabel mid = new JLabel("salut c'est cool");
+     private JLabel bot = new JLabel("contenu incorrect");
+     
      //textfield
-     private JTextField champkm = new JTextField(10);
-     private JTextField champpi = new JTextField(10);
-     private JTextField champm = new JTextField(10);
-     private JTextField champmi = new JTextField(10);
-     private JTextField champkmh = new JTextField(10);
-     private JTextField champkn = new JTextField(10);
+     private JTextField champkm = new JTextField("", 10);
+     private JTextField champpi = new JTextField("", 10);
+     private JTextField champm = new JTextField("", 10);
+     private JTextField champmi = new JTextField("", 10);
+     private JTextField champkmh = new JTextField("", 10);
+     private JTextField champkn = new JTextField("", 10);
      
      //button
      private JButton convtop = new JButton("<=>");
@@ -70,20 +76,26 @@ import javax.swing.*;
         pantop.add(km);
         pantop.add(champkm);
         pantop.add(convtop);
+        convtop.addActionListener(new kmtomi());
         pantop.add(champmi);
         pantop.add(mi);
+        reptop.add(top);
         //mid
         panmid.add(m);
         panmid.add(champm);
         panmid.add(convmid);
+        convmid.addActionListener(new mtopi());
         panmid.add(champpi);
         panmid.add(pi);
+        repmid.add(mid);
         //bot
         panbot.add(kmh);
         panbot.add(champkmh);
         panbot.add(convbot);
+        convbot.addActionListener(new kmhtokn());
         panbot.add(champkn);
         panbot.add(kn);
+        repbot.add(bot);
         
         
         
@@ -91,5 +103,92 @@ import javax.swing.*;
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
+     
+     class kmtomi implements ActionListener {
+         
+         public void actionPerformed(ActionEvent e){
+         String tkm = champkm.getText();
+         String tmi = champmi.getText();
+            //ceck si les 2 ont un contenu 
+            if (tkm.length() != 0 && tmi.length() != 0) {
+                top.setText("les 2 champs sont rempli !");
+            }
+            //transforme tkm
+            else if (Pattern.matches("[0-9.]+", tkm) == true && tkm.length() != 0){
+                double dkm = Double.parseDouble(tkm);
+                champmi.setText("" + dkm*0.621);
+                top.setText("");
+            }
+            //transforme tmi
+            else if (Pattern.matches("[0-9.]+", tmi) == true && tmi.length() != 0){
+                double dmi = Double.parseDouble(tmi);
+                champkm.setText(""+ dmi/0.621);
+                top.setText("");
+            }
+            //sinon
+            else{
+                top.setText("contenu incorrect!");
+            }         
+         }
+ 
+ }
+     
+     class mtopi implements ActionListener {
+         
+         public void actionPerformed(ActionEvent e){
+         String tm = champm.getText();
+         String tpi = champpi.getText();
+            //ceck si les 2 ont un contenu 
+            if (tm.length() != 0 && tpi.length() != 0) {
+                mid.setText("les 2 champs sont rempli !");
+            }
+            //transforme tkm
+            else if (Pattern.matches("[0-9.]+", tm) == true && tm.length() != 0){
+                double dkm = Double.parseDouble(tm);
+                champpi.setText("" + dkm*0.0254);
+                mid.setText("");
+            }
+            //transforme tmi
+            else if (Pattern.matches("[0-9.]+", tpi) == true && tpi.length() != 0){
+                double dmi = Double.parseDouble(tpi);
+                champm.setText(""+ dmi/0.0254);
+                mid.setText("");
+            }
+            //sinon
+            else{
+                mid.setText("contenu incorrect!");
+            }         
+         }
+ 
+ }
+     
+     class kmhtokn implements ActionListener {
+         
+         public void actionPerformed(ActionEvent e){
+         String tkmh = champkmh.getText();
+         String tkn = champkn.getText();
+            //ceck si les 2 ont un contenu 
+            if (tkmh.length() != 0 && tkn.length() != 0) {
+                bot.setText("les 2 champs sont rempli !");
+            }
+            //transforme tkmh
+            else if (Pattern.matches("[0-9.]+", tkmh) == true && tkmh.length() != 0){
+                double dkmh = Double.parseDouble(tkmh);
+                champkn.setText("" + dkmh*1.852);
+                bot.setText("");
+            }
+            //transforme tkn
+            else if (Pattern.matches("[0-9.]+", tkn) == true && tkn.length() != 0){
+                double dkn = Double.parseDouble(tkn);
+                champkmh.setText(""+ dkn/1.852);
+                bot.setText("");
+            }
+            //sinon
+            else{
+                bot.setText("contenu incorrect!");
+            }         
+         }
+ 
+ }
     
 }
